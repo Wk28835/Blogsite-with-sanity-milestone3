2861ed91a823d06200099e7626a16b2f40eb8985
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2025-01-10 19:24:39 +0500

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import blog from '../blog'
+import comments from '../comments'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blog],
+  types: [blog,comments],
 }
